d48708260ad81f4f981530fc06a0b5aedc6c8bb5
borboss <67179348+borboss@users.noreply.github.com>
2023-03-25 20:32:02 -0400

    use rectangles instead of 1px sprites

--- src/game/gui/mod.rs
+++ src/game/gui/mod.rs
@@ -12,35 +12,49 @@ impl Plugin for GuiPlugin {
 
 fn init_render_bar(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
     let window: &Window = window_query.get_single().unwrap();
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("sprites/red.png"),
-            transform: Transform::from_xyz(window.width() / 2.0, window.height() - 25.0, 9.9f32)
-                .with_scale(Vec3::new(0.0f32, 12.5f32, 1.0f32)),
+            sprite: Sprite {
+                color: Color::rgb(232.0/255.0, 59.0/255.0, 59.0/255.0),
+                custom_size: Some(Vec2::new(250.0, 12.5)),
+                ..default()
+            },
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() - 25.0, 9.9f32),
             ..default()
         },
         StatusBar {},
         HpMarker {},
     ));
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(70.0/255.0, 90.0/255.0, 1.0),
+                custom_size: Some(Vec2::new(250.0, 12.5)),
+                ..default()
+            },
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() - 25.0, 9.9f32),
+            ..default()
+        },
+        StatusBar {},
+        MpMarker {},
+    ));
 }
 
 fn update_health_bar(
-    mut hp_bar: Query<&mut Transform, With<HpMarker>>,
+    mut hp_bar: Query<&mut Sprite, With<HpMarker>>,
     window_query: Query<&Window, With<PrimaryWindow>>,
     player_query: Query<&HP, With<Player>>,
 ) {
     let mut _health_bar_bundle = hp_bar.get_single_mut().unwrap();
     let player_hp = player_query.get_single().unwrap();
     let window: &Window = window_query.get_single().unwrap();
-    _health_bar_bundle.scale = Vec3::new(
+    _health_bar_bundle.custom_size = Some(Vec2::new(
         250.0 * (player_hp.hp as f32 / player_hp.max_hp as f32),
         12.5f32,
-        1.0f32,
-    );
+    ));
 }
 
 #[derive(Component)]
