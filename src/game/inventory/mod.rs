96a451fc65f6efe906932818d0c0b76704eab620
borboss <67179348+borboss@users.noreply.github.com>
2023-03-25 22:00:38 -0400

    remove tweening plugin

--- src/game/inventory/mod.rs
+++ src/game/inventory/mod.rs
@@ -5,7 +5,6 @@ pub mod card_components;
 pub mod card_systems;
 mod systems;
 
-use bevy_tweening::TweeningPlugin;
 use components::*;
 use card_systems::*;
 use systems::*;
@@ -15,7 +14,6 @@ pub struct InventoryPlugin;
 impl Plugin for InventoryPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_plugin(TweeningPlugin)
             .init_resource::<Inventory>()
             .add_startup_system(init_inventory.before(init_render_cards))
             .add_startup_system(init_render_cards)
